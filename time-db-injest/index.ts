6c535e37dce3f745accae0223eb928ec3d967783
Hiperultimate <hiperultimatevegito@gmail.com>
2025-08-27 13:00:39 +0530

    Pushing price poller data to timescale db

--- time-db-injest/index.ts
+++ time-db-injest/index.ts
@@ -1,20 +1,16 @@
 import client from "./timescale_db";
 import redisClient from "./redis_db";
+import type { IPubQuotes } from "./types";
 
-const res = await client.query("SELECT version()");
-console.log("DB Version:", res.rows[0].version);
 
-await redisClient.pSubscribe("*", (item) => {
-    console.log("Redis published output : ", item);
+await redisClient.pSubscribe("*", async (item) => {
+  console.log("Redis published output : ", item);
+  const data: IPubQuotes = JSON.parse(item);
+
+  const queryText = `
+    INSERT INTO quotes (time, symbol, bid_price, ask_price)
+    VALUES (NOW(), $1, $2, $3)
+    `;
+  const value = [...Object.values(data)];
+  await client.query(queryText, value);
 });
-
-// const createTable = await client.query(`CREATE TABLE IF NOT EXISTS quotes (
-//   time        TIMESTAMPTZ       NOT NULL,
-//   symbol      TEXT              NOT NULL,
-//   bid_price   DOUBLE PRECISION  NOT NULL,
-//   ask_price   DOUBLE PRECISION  NOT NULL
-// )`);
-
-// fetch data from redis polling server 
-// subscribe to redis 
-// log the data
