75604881ea9d57e4e144c702394995b303c1f119
hiper <hiperultimatevegito@gmail.com>
2025-08-31 19:42:48 +0530

    WS feeder changes

--- ws-feeder/index.ts
+++ ws-feeder/index.ts
@@ -5,11 +5,12 @@ const wss = new WebSocketServer({ port: 6767 });
 
 redisClient.pSubscribe("*", (message, channel) => {
   console.log("Redis published:", { channel, message });
-  
+
   // Broadcast to all connected WebSocket clients
   for (const client of wss.clients) {
     if (client.readyState === client.OPEN) {
-      client.send(JSON.stringify({ channel, data: message }));
+      const sendData = JSON.parse(message);
+      client.send(JSON.stringify(sendData));
     }
   }
 });
@@ -17,13 +18,9 @@ redisClient.pSubscribe("*", (message, channel) => {
 wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
-//   ws.on("message", function message(data) {
-//     console.log("received: %s", data);
-//   });
-
   ws.on("close", () => {
-    console.log("Connection closed ws-feeder")
-  })
+    console.log("Connection closed ws-feeder");
+  });
 
   ws.send("Connected to ws-feeder");
 });
